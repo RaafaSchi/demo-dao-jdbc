95ef46ca4a1f54de9034e24ac10494066ccbc6b2
Rafael Schiavon <raafaschi@gmail.com>
2021-03-02 22:14:20 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,6 +12,8 @@ public class Program {
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
 		
+		Seller seller = new Seller(1, "Rafa", "rafa@123", new Date(), 123456.0, obj);
+		System.out.println(seller);
 	}
 
 }
