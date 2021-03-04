44d65d6afc12d945ba93756c08c61330cd611d62
Rafael Schiavon <raafaschi@gmail.com>
2021-03-03 22:23:13 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,13 +11,13 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
 
-		Seller seller = new Seller(1, "Rafa", "rafa@123", new Date(), 123456.0, obj);
-		System.out.println(seller);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 	}
 
 }
+	
